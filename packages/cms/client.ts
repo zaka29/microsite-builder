c92dd16ff4b10f8cd4b3d51891805c3bd9d2a3d2
Stanislav Zaichenko <zaka29@gmail.com>
2025-10-25 21:17:41 +1100

    sanity cms wip

--- packages/cms/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const sanity = createClient({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: "production",
-  useCdn: true,
-  apiVersion: "2025-01-01",
-});
